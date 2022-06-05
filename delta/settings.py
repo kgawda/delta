b20f77ba2c5b3288c495c33ce929f595b148210e
Konrad Gawda <k.gawda@alx.pl>
2022-06-05 15:51:21 +0200

    Support for settings_local.py

--- delta/settings.py
+++ delta/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+try:
+    from .settings_local import *
+except ModuleNotFoundError:
+    pass
